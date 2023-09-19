2446efaeb0002f7420eeaa3749f37c64b8278686
Mathias Schoettle <mathias.schoettle@st.oth-regensburg.de>
2023-09-19 13:32:15 +0200

    changed directory structure

--- src/main/java/de/mscho/toftws/flashcard/repository/FlashcardDeckRepo.java
+++ src/main/java/de/mscho/toftws/flashcard/repository/FlashcardDeckRepo.java
@@ -1,12 +1,14 @@
 package de.mscho.toftws.flashcard.repository;
 
 import de.mscho.toftws.flashcard.entity.FlashcardDeck;
+import jakarta.annotation.Nonnull;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface FlashcardDeckRepo extends CrudRepository<FlashcardDeck, Long> {
     @Override
+    @Nonnull
     List<FlashcardDeck> findAll();
     Integer deleteFlashcardDeckById(Long id);
 }
