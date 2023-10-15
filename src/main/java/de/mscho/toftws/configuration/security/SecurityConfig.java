826efa598ff7db0f06b54c2dd5da8f2f774cc15c
Mathias Schoettle <mathias.schoettle@st.oth-regensburg.de>
2023-10-15 16:12:54 +0200

    disable cors

--- src/main/java/de/mscho/toftws/configuration/security/SecurityConfig.java
+++ src/main/java/de/mscho/toftws/configuration/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -31,6 +32,7 @@ public class SecurityConfig implements WebMvcConfigurer {
                 )
                 .apply(new ApiConfigurer(userService)).and()
                 .csrf(configurer -> configurer.ignoringRequestMatchers(antMatcher("/calendar/**")))
+                .cors(AbstractHttpConfigurer::disable)
                 .build();
     }
 }
