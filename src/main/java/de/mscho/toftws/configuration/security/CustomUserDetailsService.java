ae8b583f7e26e3f3c5c9c4dd4a4b9331b08ccbc0
Mathias Schoettle <mathias.schoettle@st.oth-regensburg.de>
2023-12-21 00:55:36 +0100

    added custom user details service

---
+++ src/main/java/de/mscho/toftws/configuration/security/CustomUserDetailsService.java
@@ -0,0 +1,18 @@
+package de.mscho.toftws.configuration.security;
+
+import de.mscho.toftws.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userService.getByUsername(username).orElseThrow(() -> new UsernameNotFoundException("no user found for username"));
+    }
+}
